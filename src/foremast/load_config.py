fee78e7d05f9923a572733c6ba8231cefc9cef66
Nate Tangsurat <e4r7hbug@gmail.com>
2018-06-11 00:40:14 -0500

    refactor: Move default configuration to new file

--- src/foremast/load_config.py
+++ src/foremast/load_config.py
@@ -8,51 +8,9 @@ import os
 
 import deepmerge
 
-LOG = logging.getLogger(__name__)
+from . import DEFAULT_CONFIG
 
-DEFAULT_CONFIG = {
-    'base': {
-        'ami_json_url': None,
-        'default_ec2_securitygroups': {},
-        'default_elb_securitygroups': {},
-        'default_securitygroup_rules': {},
-        'domain': 'example.com',
-        'ec2_pipeline_types': [],
-        'envs': [],
-        'gate_api_url': None,
-        'gate_ca_bundle': '',
-        'gate_client_cert': '',
-        'git_url': None,
-        'regions': [],
-        'securitygroup_replacements': {},
-        'templates_path': None,
-        'types': [
-            'datapipeline',
-            'ec2',
-            'lambda',
-            'rolling',
-            's3',
-        ],
-    },
-    'credentials': {
-        'gitlab_token': None,
-        'slack_token': None,
-    },
-    'formats': {},
-    'headers': {
-        'accept': '*/*',
-        'content-type': 'application/json',
-        'user-agent': 'foremast',
-    },
-    'links': {},
-    'task_timeouts': {
-        'default': 120,
-        'envs': {},
-    },
-    'whitelists': {
-        'asg_whitelist': [],
-    },
-}
+LOG = logging.getLogger(__name__)
 
 CONFIG_CFG_LOCATIONS = frozenset([
     '/etc/foremast/foremast.cfg',
