4bd3c43fc77499363f653c0917bafe645f8d6b2e
Nate Tangsurat <e4r7hbug@gmail.com>
2018-06-11 00:46:13 -0500

    refactor: Move merging logic to new file

--- src/foremast/load_config.py
+++ src/foremast/load_config.py
@@ -2,13 +2,11 @@
 """Central configuration loading."""
 import configparser
 import importlib
-import json
 import logging
 import os
 
-import deepmerge
-
-from . import DEFAULT_CONFIG
+from . import merge
+from .default_config import DEFAULT_CONFIG
 
 LOG = logging.getLogger(__name__)
 
@@ -23,100 +21,6 @@ ENV_FOREMAST_CONFIG_DIRECTORY = 'FOREMAST_CONFIG_DIRECTORY'
 CONFIG_MODULE_DIRECTORY = os.getenv(ENV_FOREMAST_CONFIG_DIRECTORY, os.getenv('PWD'))
 CONFIG_MODULE_NAME = 'foremast_config'
 CONFIG_MODULE_FILE = '{0}/{1}.py'.format(CONFIG_MODULE_DIRECTORY, CONFIG_MODULE_NAME)
-
-
-class TypeConflictStrategies(deepmerge.strategy.type_conflict.TypeConflictStrategies):
-    """Add conflict strategy."""
-
-    @staticmethod
-    def strategy_comma_split_append(config, path, base, nxt):
-        """Merge comma separated :obj:`str` and :obj:`list`."""
-        if isinstance(base, str) and isinstance(nxt, list):
-            value = base.split(',') + nxt
-        elif isinstance(base, list) and isinstance(nxt, str):
-            value = base + nxt.split(',')
-        else:
-            value = deepmerge.strategy.core.STRATEGY_END
-        return value
-
-    @staticmethod
-    def strategy_not_empty(config, path, base, nxt):
-        """Return whichever value is not empty."""
-        if not base and nxt:
-            value = nxt
-        elif base and not nxt:
-            value = base
-        else:
-            value = deepmerge.strategy.core.STRATEGY_END
-        return value
-
-    @staticmethod
-    def strategy_configparser(config, path, base, nxt):
-        """Convert :obj:`configparser.SectionProxy` to :obj:`dict` for merge."""
-        value = deepmerge.strategy.core.STRATEGY_END
-        if isinstance(base, configparser.SectionProxy):
-            value = Merge().merge(dict(base), nxt)
-        elif isinstance(nxt, configparser.SectionProxy):
-            value = Merge().merge(base, dict(nxt))
-        return value
-
-    @staticmethod
-    def strategy_override_str_to_int(config, path, base, nxt):
-        """Convert :obj:`str` to :obj:`int` for comparison."""
-        value = deepmerge.strategy.core.STRATEGY_END
-        if isinstance(base, int) and isinstance(nxt, str):
-            value = int(nxt)
-        return value
-
-    @staticmethod
-    def strategy_json_security_groups(config, path, base, nxt):
-        """Try to convert JSON to Python object of Security Groups."""
-        value = deepmerge.strategy.core.STRATEGY_END
-        if isinstance(base, dict) and isinstance(nxt, str):
-            try:
-                code = json.loads(nxt)
-            except json.JSONDecodeError:
-                code = {'all_envs': nxt.split(',')}
-            value = Merge().merge(base, code)
-        return value
-
-
-class FallbackStrategies(deepmerge.strategy.fallback.FallbackStrategies):
-    """Add fallback strategies when Types are unhandled by builtins."""
-
-    @staticmethod
-    def strategy_equality(config, path, base, nxt):
-        """Check for equality and return first one."""
-        value = deepmerge.strategy.core.STRATEGY_END
-        if base == nxt:
-            value = base
-        return value
-
-
-class Merge(deepmerge.Merger):
-    """Handle Foremast configuration legacy values."""
-
-    def __init__(self):
-        super().__init__(
-            type_strategies=[
-                (list, 'append'),
-                (dict, 'merge'),
-            ],
-            fallback_strategies=[],
-            type_conflict_strategies=[],
-        )
-
-        self._fallback_strategy = FallbackStrategies([
-            'equality',
-        ])
-
-        self._type_conflict_strategy = TypeConflictStrategies([
-            'comma_split_append',
-            'json_security_groups',
-            'not_empty',
-            'configparser',
-            'override_str_to_int',
-        ])
 
 
 class FrozenConfig(dict):
@@ -198,7 +102,7 @@ class ForemastConfig(object):
             locations = '\n'.join(list(CONFIG_CFG_LOCATIONS) + [CONFIG_MODULE_FILE])
             LOG.warning('No configuration files found in:\n%s\nUsing defaults.', locations)
 
-        config = Merge().merge(DEFAULT_CONFIG, loaded_config)
+        config = merge.MERGE(DEFAULT_CONFIG, loaded_config)
 
         self._config = FrozenConfig(config)
         LOG.debug('Complete configuration: %s', self._config)
---
+++ src/foremast/merge.py
@@ -0,0 +1,102 @@
+"""Extend :mod:`deepmerge` with new strategies."""
+import configparser
+import json
+
+import deepmerge
+
+
+class TypeConflictStrategies(deepmerge.strategy.type_conflict.TypeConflictStrategies):
+    """Add conflict strategy."""
+
+    @staticmethod
+    def strategy_comma_split_append(config, path, base, nxt):
+        """Merge comma separated :obj:`str` and :obj:`list`."""
+        if isinstance(base, str) and isinstance(nxt, list):
+            value = base.split(',') + nxt
+        elif isinstance(base, list) and isinstance(nxt, str):
+            value = base + nxt.split(',')
+        else:
+            value = deepmerge.strategy.core.STRATEGY_END
+        return value
+
+    @staticmethod
+    def strategy_not_empty(config, path, base, nxt):
+        """Return whichever value is not empty."""
+        if not base and nxt:
+            value = nxt
+        elif base and not nxt:
+            value = base
+        else:
+            value = deepmerge.strategy.core.STRATEGY_END
+        return value
+
+    @staticmethod
+    def strategy_configparser(config, path, base, nxt):
+        """Convert :obj:`configparser.SectionProxy` to :obj:`dict` for merge."""
+        value = deepmerge.strategy.core.STRATEGY_END
+        if isinstance(base, configparser.SectionProxy):
+            value = Merge().merge(dict(base), nxt)
+        elif isinstance(nxt, configparser.SectionProxy):
+            value = Merge().merge(base, dict(nxt))
+        return value
+
+    @staticmethod
+    def strategy_override_str_to_int(config, path, base, nxt):
+        """Convert :obj:`str` to :obj:`int` for comparison."""
+        value = deepmerge.strategy.core.STRATEGY_END
+        if isinstance(base, int) and isinstance(nxt, str):
+            value = int(nxt)
+        return value
+
+    @staticmethod
+    def strategy_json_security_groups(config, path, base, nxt):
+        """Try to convert JSON to Python object of Security Groups."""
+        value = deepmerge.strategy.core.STRATEGY_END
+        if isinstance(base, dict) and isinstance(nxt, str):
+            try:
+                code = json.loads(nxt)
+            except json.JSONDecodeError:
+                code = {'all_envs': nxt.split(',')}
+            value = Merge().merge(base, code)
+        return value
+
+
+class FallbackStrategies(deepmerge.strategy.fallback.FallbackStrategies):
+    """Add fallback strategies when Types are unhandled by builtins."""
+
+    @staticmethod
+    def strategy_equality(config, path, base, nxt):
+        """Check for equality and return first one."""
+        value = deepmerge.strategy.core.STRATEGY_END
+        if base == nxt:
+            value = base
+        return value
+
+
+class Merge(deepmerge.Merger):
+    """Handle Foremast configuration legacy values."""
+
+    def __init__(self):
+        super().__init__(
+            type_strategies=[
+                (list, 'append'),
+                (dict, 'merge'),
+            ],
+            fallback_strategies=[],
+            type_conflict_strategies=[],
+        )
+
+        self._fallback_strategy = FallbackStrategies([
+            'equality',
+        ])
+
+        self._type_conflict_strategy = TypeConflictStrategies([
+            'comma_split_append',
+            'json_security_groups',
+            'not_empty',
+            'configparser',
+            'override_str_to_int',
+        ])
+
+
+MERGE = Merge().merge
